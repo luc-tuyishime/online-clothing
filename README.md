6d6ce9b9d923c0cbe9bf8d13c533b75d5c330353
Jean luc <luctunechi45@gmail.com>
2020-04-21 00:53:08 +0200

    add new changes

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Online Clothing
 
 React - Firebase - Redux - Hooks - Context API - GraphQL - Stripe
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import HomePage from './homepage.component';
-import './homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
