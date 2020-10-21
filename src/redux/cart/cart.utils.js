3dc9124a0afa555cadc23ce72d03fbdaa283bac1
@luc-tuyishimee <luctunechi45@gmail.com>
2020-10-22 01:09:56 +0200

    add addItem and removeItem to checkout component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // IF not exist return the item with quanity = 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+// Decrease or Remove ITEM
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    // Check if Item exist by Find
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    // Check if CartIten quantity === 1
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map((cartItem) => {
+        // IF cartItem.id === cartItemToremove.id decrease the object quantity - 1
+        // By doing immutability, spread everything inside cartItem and update the quantity
+        return cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem;
+    });
+};
